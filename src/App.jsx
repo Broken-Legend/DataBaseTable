b4cab6a66a512156422c72f182cc16edc2833fa6
Chris Walker <chriswalkeralt@gmail.com>
2024-10-23 11:08:53 +0100

    Modified table with searchbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 import Paper from '@mui/material/Paper';
 import axios from 'axios';
 
@@ -35,6 +37,16 @@ export default function BasicTable() {
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
          <TableHead>
           <TableRow>
+          <Box
+            component="form"
+            sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
+            noValidate
+            autoComplete="off"
+          >
+            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+            <TextField id="filled-basic" label="Filled" variant="filled" />
+            <TextField id="standard-basic" label="Standard" variant="standard" />
+          </Box>
             <TableCell>ID</TableCell>
             <TableCell align="right">FamiliaEmms</TableCell>
             <TableCell align="right">Tipo</TableCell>
